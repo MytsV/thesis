4eb498c495f1658f2a0400cc475ac610ef70a175
MytsV <mytsvictoria@gmail.com>
2025-04-07 16:12:24 +0300

    Connect the backend FastAPI application to PostgreSQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+from json import dumps
+
+from fastapi import FastAPI, Depends
 from typing import Dict
+
+from sqlalchemy.orm import Session
+
+from app.sqla.database import get_db
+import app.sqla.models as models
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root() -> Dict[str, str]:
-    return {"message": "Hello World!"}
+async def root(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    user_list = [
+        {"id": user.id, "username": user.username, "email": user.email}
+        for user in users
+    ]
+    return user_list
---
+++ backend/app/sqla/database.py
@@ -0,0 +1,41 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+DB_NAME = os.getenv("POSTGRES_DB")
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True,
+)
+
+SessionLocal = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
+
+Base = declarative_base()
+Base.query = SessionLocal.query_property()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
