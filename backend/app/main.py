00e580a2003b7c31cb0556a081ffba5fbd5f2f7a
MytsV <mytsvictoria@gmail.com>
2025-04-16 17:45:43 +0300

    Implement user-friendly display of validation errors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,42 @@
 import os
-from typing import List
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 
-from sqlalchemy.orm import Session
-
-from app.models.user_models import UserCreateResponse
 from app.routes import auth, test
-from app.sqla.database import get_db
-import app.sqla.models as models
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 app = FastAPI()
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    default_message = "Validation failed"
+
+    if exc.errors():
+        error = exc.errors()[0]
+        message = error["msg"]
+        field_location = error.get("loc", [])
+        field_name = field_location[-1] if field_location else None
+
+        if field_name and message:
+            clean_message = message.replace("Value error, ", "")
+            full_message = f"{field_name}: {clean_message}"
+        else:
+            full_message = default_message
+
+        return JSONResponse(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"detail": full_message},
+        )
+    else:
+        return JSONResponse(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"detail": default_message},
+        )
+
 
 app.include_router(auth.router)
 app.include_router(test.router)
